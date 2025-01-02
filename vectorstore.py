fe5be822800b261b0bb1c9f6c8589a81d040084d
Amarta Waghani <amartawaghani@gmail.com>
2025-01-02 19:59:50 +0500

    Create vectorstore.py

---
+++ vectorstore.py
@@ -0,0 +1,20 @@
+from embedding import embed_model
+from data_processing import doc_list
+from qdrant_client import qdrant_client
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv, dotenv_values
+import os
+
+load_dotenv()
+
+def create_retriever(doc_list, embed_model):
+    qdrant = QdrantVectorStore.from_documents(
+    doc_list,
+    embed_model,
+    url = os.getenv("qdrant_url"),
+    api_key = os.getenv("qdrant_key"),
+    collection_name = "Steve_McConnel_code_book"
+    )
+
+    retriever = qdrant.as_retriever()
+    return retriever
